42cd1b257c3360dd000d2613e3c83b27270d2cbb
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2020-12-16 23:06:57 +0200

    new file

--- js3/lap06.js
+++ js3/lap06.js
@@ -7,58 +7,13 @@ var numTow = prompt("Enter Your height");
 alert("You Are Perfect! Give Love to Your-Self ");
 
 function know() {
-    var answer = prompt("Are you satisfide with your-self? ");
-    if(answer === "yes") {
-      alert("You are Awesome:D Keep it up!");
-    } else {
-      alert("You should be =) You are amazing!");
+    var answer = prompt("Do You enjoy cooking?");
+    while(answer !== "yes") {
+      alert("You should be!!");
+      answer = prompt("does food make you happy?");
     }
+    alert ("You arrived to you destenation =D")
   }
-know(); 
-
-let cooking = function() {
-  let cooking = prompt("Enter the type of food that you want: Italian ? Arabian ? Indian ?");
-  let dish ="";
-
-  while(cooking !== "Italian" && cooking !== "Arabian" && cooking !== "Indian") {
-    cooking = prompt("nter the type of food that you want: Italian ? Arabian ? Indian ?");
-  }
-
-  if(cooking === "Italian") {
-    dish = '<'
-  } else if (cooking === "aArabian") {
-    dish = 'MANSAF'
-  } else if (cooking === "Indian") {
-    dish = 'chappati with kabab'
-  }
-
-  return dish;
-}
-
-let howMany = function () {
-  let count = prompt("How many cat do you want?");
-
-  while(count === "" || isNaN(count)) {
-    count = prompt("Please enter the number of cats you want!!");
-  }
-
-  return count;
-}
-
-let showCat = function() {
-  let cat = getCat();
-  let total = howMany();
-  let result = "";
-
-  for(let i = 0; i < total; i++) {
-    result = result + '<p>cat number #' + i + " " + cat + '</p>'
-  }
-
-  alert("Please scroll down to see your cat/s");
-  return result;
-}
-
-
-
+know();
 
 

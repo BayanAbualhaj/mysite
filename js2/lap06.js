ac6c670f7c9af00358ef746e6c072d7c0b4e64f4
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2020-12-16 20:50:24 +0200

    final

--- js2/lap06.js
+++ js2/lap06.js
@@ -6,9 +6,15 @@ var numOne= prompt("Enter Your weight:  ");
 var numTow = prompt("Enter Your height");
 alert("You Are Perfect! Give Love to Your-Self ");
 
-
-
-   
+function know() {
+    var answer = prompt("Are you satisfide with your-self? ");
+    if(answer === "yes") {
+      alert("You are Awesome:D Keep it up!");
+    } else {
+      alert("You should be =) You are amazing!");
+    }
+  }
+know(); 
 
 
 

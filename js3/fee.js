eb008a7f2ad5612b7317780765c924bc752a4402
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2020-12-17 17:28:50 +0200

    new

--- js3/fee.js
+++
@@ -1,7 +0,0 @@
-
-
-
-var answer = ''; 
-while (answer !== 'S3kreT P455w0rD') {
-  answer = prompt('Enter the passphrase to proceed...');
-} 
--- js3/lap06.js
+++ js3/lap06.js
@@ -16,4 +16,49 @@ function know() {
   }
 know();
 
+function select() {
+  let getdish = function(){
+    let userInput= prompt("Select your dish: pasta / mansaf / msakhan / chicken");
+    let dish= "";
 
+    while(userInput !== "pasta" && userInput !== "mansaf" && userInput !== "chicken" && userInput !== "msakhan" ){
+      userInput = prompt("Please select you dish: pasta / mansaf / msakhan / chicken");
+    }
+
+    if (userInput=== "chicken"){
+      dish='<img src="chekparm.jpg">'
+    } else if (userInput==="mansaf"){
+      dish= '<img src="download.jpg">'
+    } else if (userInput=== "msakhan"){
+      dish= '<img src="ms5n.jpg"'
+    } else if (userInput==="pasta"){
+      dish= '<img src="feettch.jpg">'
+    }
+    return dish;
+  }
+
+  let howMany = function () {
+    let count = prompt("How many dishes do you want to cook?");
+  
+    while(count === "" || isNaN(count)) {
+      count = prompt("Please enter the number of dishes do you want to cook!");
+    }
+  
+    return count;
+  }
+
+  let dishpic = function() {
+    let dish = getdish();
+    let total = howMany();
+    let result = "";
+  
+    for(let i = 0; i < total; i++) {
+      result = result + '<p>dish number #' + i + " " + dish + '</p>'
+    }
+  
+    alert("scroll down to see your dish/es");
+    return result;
+  }
+}
+select();
+ 
